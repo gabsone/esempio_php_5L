511c6d41a463df4030a58f16626b8fff72897f40
gabsone <g.ciliberti@lineargruppo.it>
2026-02-13 10:48:20 +0100

    Add SQL script to create and populate users table

---
+++ SQL/init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE utenti(
+                       id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                       nome VARCHAR(256) NOT NULL,
+                       cognome VARCHAR(256) NOT NULL,
+                       email VARCHAR(256) NOT NULL,
+                       passwd VARCHAR(256) NOT NULL,
+                       abilitato BOOLEAN default true,
+                       data_ultimo_accesso DATETIME
+);
+
+ALTER TABLE utenti RENAME TO users;
+ALTER TABLE users ADD CONSTRAINT constr_email UNIQUE(email);
+
+INSERT INTO users(nome, cognome, email, passwd, abilitato) VALUES
+("Gabriele", "Ciliberti","gc@email.it",MD5("pippo"), true),
+("Andrea", "Vischi","av@email.it",MD5("pluto"), true),
+("Adele", "Volpe","adv@email.it",MD5("paperino"), true);
+
